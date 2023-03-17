3908dfd5a569c64733bf63f10e5d27c308a69fd4
TechRavi67 <ravidb67@gmail.com>
2023-03-17 12:00:52 +0530

    first commit

--- script.js
+++ script.js
@@ -1,48 +1,15 @@
+const display = (val) => {
+  document.getElementById('display').value += val;
+  return val;
+}
 
-const searchData = () => {
-    let countryName = document.getElementsByClassName("country-name")[0].value;
-    var result = document.getElementsByClassName("output")[0].innerHTML= '';
-    if (countryName != "") {
-      fetch(`https://restcountries.com/v3.1/name/${countryName}`) 
-      .then((info) => info.json())     
-        .then((data) => {
-          debugger
-          let country = data[0];
-          let currency = Object.values(country.currencies);
-          let language = Object.values(country.languages);
-          document.getElementsByClassName("output")[0].innerHTML= `
-              <h2>${country.name.common}</h2>
-              <div><img  class="flag" src="${country.flags.svg}"</div>
-              <table>
-              <tr>
-                  <td>Capital : </td>
-                  <td>${country.capital}</td>
-              </tr>
-              <tr>
-                  <td>Currency : </td>
-                  <td>${currency[0].name} ( ${currency[0].symbol} )</td>
-              </tr>
-              <tr>
-                  <td>Languages : </td>
-                  <td>${language}</td>
-              </tr>
-              <tr>
-                  <td>Area : </td>
-                  <td>${country.area}</td>
-              </tr>
-              <tr>
-                  <td>Population : </td>
-                  <td>${country.population}</td>
-              </tr>
-              </table>
-              `;
-        })
-      //   .catch((error) => {
-      //     document.getElementsByClassName("output")[0].innerHTML= `<div>${error}</div>`;
-      //   })
-      
-    } else {
-      document.getElementsByClassName("output")[0].innerHTML= "Please enter valid data.";
-    }
-  };
-  
+const solve = () => {
+  let x = document.getElementById('display').value;
+  let y = eval(x);
+  document.getElementById('display').value = y;
+  return y;
+}
+
+const clearScreen = () => {
+  document.getElementById('display').value = '';
+}
